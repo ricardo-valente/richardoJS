dabf4f67b8f1e016a8e6b58ac8718ab634c4e688
Ricardo Valente <ricardo.valente@talkdesk.com>
2018-09-09 18:52:57 +0100

    Added views and static dirs

--- dev/server/index.js
+++ dev/server/index.js
@@ -13,10 +13,16 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
+app.set('view engine', 'pug')
+app.set('views', config.dir('views').path)
+app.use('/static', express.static(config.dir('static').path))
 
 // Routes
 app.get('/', (request, response) => {
-  response.send('Hello Richardo!')
+  response.render(
+    path.join(config.dir('views').path, 'index.pug'),
+    config)
 })
 
 app.listen(config.server.port)
+console.log(`${config.app.title} running on ${config.app.url}!`)
